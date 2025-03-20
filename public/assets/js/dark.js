19ad0e91d9a2816a06f3ca8be51b97e2c466c007
Juwan Hollingsworth <juwan.txt@gmail.com>
2025-03-19 23:16:15 -0400

    fix scss issue with case studies

--- public/assets/js/dark.js
+++ public/assets/js/dark.js
@@ -1,1 +1,25 @@
-(()=>{function t(){document.body.classList.add("dark-mode"),localStorage.setItem("theme","dark")}function o(){document.body.classList.remove("dark-mode"),localStorage.setItem("theme","light")}function a(){let e="light";localStorage.getItem("theme")?e=localStorage.getItem("theme"):window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches&&(e="dark"),e==="dark"?t():o()}a();document.getElementById("dark-mode-toggle").addEventListener("click",()=>{localStorage.getItem("theme")==="light"?t():o()});})();
+(() => {
+  // src/assets/js/dark.js
+  function enableDarkMode() {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  }
+  function disableDarkMode() {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+  function detectColorScheme() {
+    let theme = "light";
+    if (localStorage.getItem("theme")) {
+      theme = localStorage.getItem("theme");
+    } else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      theme = "dark";
+    }
+    theme === "dark" ? enableDarkMode() : disableDarkMode();
+  }
+  detectColorScheme();
+  document.getElementById("dark-mode-toggle").addEventListener("click", () => {
+    localStorage.getItem("theme") === "light" ? enableDarkMode() : disableDarkMode();
+  });
+})();
+//# sourceMappingURL=dark.js.map
