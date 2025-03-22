55f02169c2d573d3a13a3657ad6eaa17319e3baa
Juwan Hollingsworth <juwan.txt@gmail.com>
2025-03-22 12:25:29 -0400

    replace titles

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
+    name: "Spark Social Media Agency",
     email: "help@codestitch.app",
     phoneForTel: "555-779-4407",
     phoneFormatted: "(555) 779-4407",
